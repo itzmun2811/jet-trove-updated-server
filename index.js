537ccd9ecaf21b0f3db62cd8aefebd32c79d3ec6
itzmun2811 <sheulyaramunni@gmail.com>
2025-06-09 20:32:12 +0600

    implement  post method to add packag te

--- index.js
+++ index.js
@@ -21,14 +21,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+// Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+ 
+    const tourPackageCollections = client.db('tourPackagesCollection').collection('tourPackage')
+
+  app.post('/addPackage',async(req,res)=>{
+    const package =req.body;
+    const result =await tourPackageCollections.insertOne(package);
+    res.send(result)
+  })
+
+
+
+
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
