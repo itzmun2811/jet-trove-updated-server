fd065f86c2b1bdfd5d4a1039b706c8188fbc3531
itzmun2811 <sheulyaramunni@gmail.com>
2025-06-11 13:11:18 +0600

    adding package details to the server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 require('dotenv').config();
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
 const port =process.env.PORT || 3000;
 
@@ -32,6 +32,18 @@ async function run() {
     res.send(result)
   })
 
+  app.get('/addPackage',async(req,res)=>{
+    const result=await tourPackageCollections.find().toArray();
+    res.send(result)
+  })
+app.get('packageDetails/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query ={
+    _id: new ObjectId(id)
+    }
+    const result=await tourPackageCollections.findOne();
+    res.send(result)
+})
 
 
 
